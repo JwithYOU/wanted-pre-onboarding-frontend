bfc4cd189958f48ae846f06b165d8ff5e24181a1
YOU <khy960101@gmail.com>
2023-04-09 01:19:19 +0900

    feat: 토큰없이 /todo에 접속시 /signin으로 리다이렉트 기능 구현

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,4 +1,14 @@
+import { useState, useEffect } from "react";
+
 const Todo = () => {
+  const [jwt, setJwt] = useState("");
+
+  useEffect(() => {
+    setJwt(localStorage.getItem("JWT"));
+    if (!jwt) {
+      return (window.location.href = "/signin");
+    }
+  }, [jwt]);
   return <h1>Welcome to Todo list</h1>;
 };
 
